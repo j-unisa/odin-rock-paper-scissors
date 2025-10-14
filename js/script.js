1487d81e39d05c4b45c523aedad61383f62643b1
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-10-14 14:23:04 -0700

    feat: add randomNumber()
    Reference function in pseudocode.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ console.log("Hello World!");
 
 function getComputerChoice() {
     /*
-    Get random number
+    Get random number from randomNumber()
     If random number is 0, 1, 2 then return rock, paper, scissors respectively
     */
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 3);
 }
