a01cd304d3dc5efda6f829e5b53e9bf2d174bb9f
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-10-14 13:41:48 -0700

    chore: add pseudocode for getComputerChoice()
    Outline steps for generating a random number and mapping it to rock,
    paper, or scissors.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+    /*
+    Get random number
+    If random number is 0, 1, 2 then return rock, paper, scissors respectively
+    */
+}
