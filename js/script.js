10f82bb7b9e9e44a5c09fa252004ec42bd66fb6d
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-10-15 09:47:56 -0700

    feat: implement getHumanChoice and add tests
    Add function call tests.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log("Hello World!");
 function getComputerChoice() 
 {
     // Get random number from randomNumber()
+    // Store in variable
     let num = randomNumber();
 
     // If random number is 0, 1, 2 then return rock, paper, scissors respectively
@@ -25,10 +26,19 @@ function randomNumber()
     return Math.floor(Math.random() * 3);
 }
 
+console.log(getComputerChoice());
+
 function getHumanChoice()
 {
     // Prompt user to pick rock, paper, or scissors
     // Store in variable
+    let choice = prompt("Choose one: rock, paper, scissors");
+
     // Return variable
+    return choice;
 }
+
+console.log(getHumanChoice());
+
 // TODO: Call getComputerChoice
+// TODO: Call getHumanChoice
