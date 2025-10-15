e90c4b7527455202059cdf13a5acbd0a12cf78b8
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-10-15 09:32:44 -0700

    feat: implement getComputerChoice()

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 console.log("Hello World!");
 
-function getComputerChoice() {
-    /*
-    Get random number from randomNumber()
-    If random number is 0, 1, 2 then return rock, paper, scissors respectively
-    */
+function getComputerChoice() 
+{
+    // Get random number from randomNumber()
+    let num = randomNumber();
+
+    // If random number is 0, 1, 2 then return rock, paper, scissors respectively
+    if (num === 0)
+    {
+        return "rock";
+    }
+    else if (num === 1)
+    {
+        return "paper";
+    }
+    else 
+    {
+        return "scissors";
+    }
 }
 
-function randomNumber() {
+function randomNumber() 
+{
     return Math.floor(Math.random() * 3);
 }
+
+// TODO: Call getComputerChoice
